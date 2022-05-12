2b59b81874a8564a7533a976645df2fb281a944d
Aisha Noori <aisha.noori2000@gmail.com>
2022-05-12 15:58:43 +0200

    added playList

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,21 +1,27 @@
-const [players, setPlayers] = useState([]);
-useEffect(() => {
-  const loadData = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_HOST}/players`
-      );
-      setPlayers(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  loadData();
-}, []);
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-return (
-  <div>
-    <h1>Playerlist</h1>
-    {JSON.stringify(players)}
-  </div>
-);
+export default function PlayList() {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_HOST}/players`
+        );
+        setPlayers(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loadData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Playerlist</h1>
+      {JSON.stringify(players)}
+    </div>
+  );
+}
